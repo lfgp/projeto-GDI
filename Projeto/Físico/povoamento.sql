a360a372ee9100e30cf25fd2eb8b7e268a7e195d
lfgp <48449255+lfgp@users.noreply.github.com>
2025-03-07 17:04:48 -0300

    Update povoamento.sql

--- Projeto/Físico/povoamento.sql
+++ Projeto/Físico/povoamento.sql
@@ -16,4 +16,10 @@ INSERT INTO REGIAO VALUES ('3','-12591021','-55696501','AMAZÔNIA,CERRADO,PANTAN
 INSERT INTO REGIAO VALUES ('4','-23585479','-46464828','CERRADO,MATA ATLÂNTICA');
 INSERT INTO REGIAO VALUES ('5','-30405098','-55602166','MATA ATLÂNTICA,PAMPA');
 
-
+INSERT INTO ESPECIE VALUES ('Panthera onca','onça pintada, jaguar','Animália','Chordata','Mammalia','Carnivora','Felidae','Panthera');
+INSERT INTO ESPECIE VALUES ('Handroanthus albus','aipê, ipê-amarelo, ipê-ouro','Plantae','Magnoliophyta','Magnoliopsida','Lamiales','Bignoniaceae','Handroanthus');
+INSERT INTO ESPECIE VALUES ('Caiman latirostris','jacaré do papo amarelo','Animália','Chordata','Reptilia','Crocodylia','Alligatoridae','Caiman');
+INSERT INTO ESPECIE VALUES ('Amaranthus viridis','bredo, caruru','Plantae','Magnoliophyta','Magnoliopsida','Caryophyllales','Amaranthaceae','Amaranthus');
+INSERT INTO ESPECIE VALUES ('Rhea americana','ema, nandu, guaripé','Animalia','Chordata','Ave','Struthioniformes','Rheidae','Rhea');
+INSERT INTO ESPECIE VALUES ('Paullinia cupana','guaranazeiro, guaraná','Plantae','Magnoliophyta','Magnoliopsida','Sapindales','Sapindaceae','Paullinia');
+INSERT INTO ESPECIE VALUES ('Myloplus sauron','piranha vegetariana','Animalia','Chordata','Actinopterygii','Characiformes','Serrasalmidae','Myloplus');
