3ab627c1ccb3fd72e69e2deb8562dbba1815fc6b
lfgp <48449255+lfgp@users.noreply.github.com>
2025-03-15 19:45:18 -0300

    rearrumando

--- Projeto/Físico/povoamento.sql
+++ Projeto/Físico/povoamento.sql
@@ -16,15 +16,25 @@ INSERT INTO REGIAO VALUES ('3','-12591021','-55696501','AMAZÔNIA,CERRADO,PANTAN
 INSERT INTO REGIAO VALUES ('4','-23585479','-46464828','CERRADO,MATA ATLÂNTICA');
 INSERT INTO REGIAO VALUES ('5','-30405098','-55602166','MATA ATLÂNTICA,PAMPA');
 
-INSERT INTO ESPECIE VALUES ('Panthera onca','onça pintada, jaguar','Animália','Chordata','Mammalia','Carnivora','Felidae','Panthera');
-INSERT INTO ESPECIE VALUES ('Handroanthus albus','aipê, ipê-amarelo, ipê-ouro','Plantae','Magnoliophyta','Magnoliopsida','Lamiales','Bignoniaceae','Handroanthus');
-INSERT INTO ESPECIE VALUES ('Caiman latirostris','jacaré do papo amarelo','Animália','Chordata','Reptilia','Crocodylia','Alligatoridae','Caiman');
-INSERT INTO ESPECIE VALUES ('Amaranthus viridis','bredo, caruru','Plantae','Magnoliophyta','Magnoliopsida','Caryophyllales','Amaranthaceae','Amaranthus');
-INSERT INTO ESPECIE VALUES ('Rhea americana','ema, nandu, guaripé','Animalia','Chordata','Ave','Struthioniformes','Rheidae','Rhea');
-INSERT INTO ESPECIE VALUES ('Paullinia cupana','guaranazeiro, guaraná','Plantae','Magnoliophyta','Magnoliopsida','Sapindales','Sapindaceae','Paullinia');
-INSERT INTO ESPECIE VALUES ('Myloplus sauron','piranha vegetariana','Animalia','Chordata','Actinopterygii','Characiformes','Serrasalmidae','Myloplus');
-INSERT INTO ESPECIE VALUES ('Anodorhynchus hyacinthinus','arara azul, ararinha azul','Animália','Chordata','Aves','Psittaciformes','Psittacidae','Anodorhynchus');
-INSERT INTO ESPECIE VALUES ('Paubrasilia echinata','pau-brasil, pau-de-pernambuco','Plantae','	Magnoliophyta','Magnoliopsida','Fabales','Fabaceae','Paubrasilia');
+INSERT INTO ESPECIE VALUES ('Panthera onca','Animália','Chordata','Mammalia','Carnivora','Felidae','Panthera');
+INSERT INTO ESPECIE VALUES ('Handroanthus albus','Plantae','Magnoliophyta','Magnoliopsida','Lamiales','Bignoniaceae','Handroanthus');
+INSERT INTO ESPECIE VALUES ('Caiman latirostris','Animália','Chordata','Reptilia','Crocodylia','Alligatoridae','Caiman');
+INSERT INTO ESPECIE VALUES ('Amaranthus viridis','Plantae','Magnoliophyta','Magnoliopsida','Caryophyllales','Amaranthaceae','Amaranthus');
+INSERT INTO ESPECIE VALUES ('Rhea americana','Animalia','Chordata','Ave','Struthioniformes','Rheidae','Rhea');
+INSERT INTO ESPECIE VALUES ('Paullinia cupana','Plantae','Magnoliophyta','Magnoliopsida','Sapindales','Sapindaceae','Paullinia');
+INSERT INTO ESPECIE VALUES ('Myloplus sauron','Animalia','Chordata','Actinopterygii','Characiformes','Serrasalmidae','Myloplus');
+INSERT INTO ESPECIE VALUES ('Anodorhynchus hyacinthinus','Animália','Chordata','Aves','Psittaciformes','Psittacidae','Anodorhynchus');
+INSERT INTO ESPECIE VALUES ('Paubrasilia echinata','Plantae','	Magnoliophyta','Magnoliopsida','Fabales','Fabaceae','Paubrasilia');
+
+INSERT INTO NOMEPOPULAR VALUES ('Panthera onca','onça pintada, jaguar');
+INSERT INTO ESPECIE VALUES ('Handroanthus albus','aipê, ipê-amarelo, ipê-ouro');
+INSERT INTO ESPECIE VALUES ('Caiman latirostris','jacaré do papo amarelo');
+INSERT INTO ESPECIE VALUES ('Amaranthus viridis','bredo, caruru');
+INSERT INTO ESPECIE VALUES ('Rhea americana','ema, nandu, guaripé');
+INSERT INTO ESPECIE VALUES ('Paullinia cupana','guaranazeiro, guaraná');
+INSERT INTO ESPECIE VALUES ('Myloplus sauron','piranha vegetariana');
+INSERT INTO ESPECIE VALUES ('Anodorhynchus hyacinthinus','arara azul, ararinha azul');
+INSERT INTO ESPECIE VALUES ('Paubrasilia echinata','pau-brasil, pau-de-pernambuco');
 
 INSERT INTO ESTUDA VALUES ('1111','1','Panthera onca',TO_DATE('12/03/2023','DD/MM/YYYY'));
 INSERT INTO ESTUDA VALUES ('2222','2','Amaranthus viridis',TO_DATE('10/05/2022','DD/MM/YYYY'));
